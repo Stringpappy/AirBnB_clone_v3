dfc7440640247402dc788ad4e0729fdad2bdc1d3
stringpappy <jejeodunayo70@gmail.com>
2024-08-05 13:45:22 -0700

    airbnb clone 3

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -65,6 +65,19 @@ class FileStorage:
             if key in self.__objects:
                 del self.__objects[key]
 
+     def get(self, cls, id):
+        """Func that Retrieve object by class and id
+        """
+        if cls in classes.values():
+            return self.__session.query(cls).filter(cls.id == id).first()
+        else:
+            return None
+
+    def count(self, cls=None):
+        """Func that Count number of objects in storage
+        """
+        return len(self.all(cls))
+
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
