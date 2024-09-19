0f6153f41612da3a8c78f87065aaddbe8033644d
Stringpappy <jejeodunayo70@gmail.com>
2024-09-18 22:41:57 -0700

    rest API

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -72,16 +72,14 @@ class DBStorage:
         self.__session = Session
 
      def get(self, cls, id):
-        """Func that Retrieve object by class and id
-        """
+        """Func that Retrieve object by class and id"""
         if cls in classes.values():
             return self.__session.query(cls).filter(cls.id == id).first()
         else:
             return None
 
     def count(self, cls=None):
-        """Func that Count number of objects in storage
-        """
+        """Func that Count number of objects in storage"""
         return len(self.all(cls))
 
     def close(self):
